985ee8172da181935ae28d3bf703ee5381034cbd
rainrainrainbow <1448282415@qq.com>
2026-07-20 00:58:30 +0800

    chore: update package to com.starcompanion (download)

--- android/app/src/main/java/com/pocketpalai/download/DownloadPackage.kt
+++ android/app/src/main/java/com/pocketpalai/download/DownloadPackage.kt
@@ -1,11 +1,11 @@
-package com.pocketpal.download
+package com.starcompanion.download
 
 import com.facebook.react.TurboReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.model.ReactModuleInfo
 import com.facebook.react.module.model.ReactModuleInfoProvider
-import com.pocketpal.specs.NativeDownloadModuleSpec
+import com.starcompanion.specs.NativeDownloadModuleSpec
 
 class DownloadPackage : TurboReactPackage() {
   override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
