ddf171766c6dbf6dfd09a8d018479fb70fcb5850
rainrainrainbow <1448282415@qq.com>
2026-07-20 00:56:51 +0800

    chore: update package to com.starcompanion

--- android/app/src/main/java/com/pocketpalai/HardwareInfoModule.kt
+++ android/app/src/main/java/com/pocketpalai/HardwareInfoModule.kt
@@ -1,8 +1,8 @@
-package com.pocketpal
+package com.starcompanion
 
 import com.facebook.react.bridge.*
 import com.facebook.react.module.annotations.ReactModule
-import com.pocketpal.specs.NativeHardwareInfoSpec
+import com.starcompanion.specs.NativeHardwareInfoSpec
 import android.os.Build
 import android.os.Debug
 import java.io.File
