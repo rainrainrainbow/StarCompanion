8be510203976a4789d881f460321e17c9f20c563
rainrainrainbow <1448282415@qq.com>
2026-07-20 00:56:55 +0800

    chore: update package to com.starcompanion

--- android/app/src/main/java/com/pocketpalai/HardwareInfoPackage.kt
+++ android/app/src/main/java/com/pocketpalai/HardwareInfoPackage.kt
@@ -1,11 +1,11 @@
-package com.pocketpal
+package com.starcompanion
 
 import com.facebook.react.TurboReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.model.ReactModuleInfo
 import com.facebook.react.module.model.ReactModuleInfoProvider
-import com.pocketpal.specs.NativeHardwareInfoSpec
+import com.starcompanion.specs.NativeHardwareInfoSpec
 
 class HardwareInfoPackage : TurboReactPackage() {
   override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
